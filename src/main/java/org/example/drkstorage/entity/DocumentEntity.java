3576f6db29f347ba7aa7ba366d41b60bd9c5d4ce
Viktar Chaikouski <viktar_chaikouski@epam.com>
2024-04-18 07:30:30 +0300

    Removed unused imports

--- src/main/java/org/example/drkstorage/entity/DocumentEntity.java
+++ src/main/java/org/example/drkstorage/entity/DocumentEntity.java
@@ -1,10 +1,8 @@
 package org.example.drkstorage.entity;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.time.Instant;
